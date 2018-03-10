7fbd59b59122e31d58c661ed06dc05f3cafef40d
RafalJDev <jaszczynski.rafal@gmail.com>
2018-03-10 22:53:31 +0100

    First assigment and first section lectures

---
+++ src/app/raf-paf-component/raf-paf-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raf-paf-component',
+  templateUrl: './raf-paf-component.component.html',
+  styleUrls: ['./raf-paf-component.component.css']
+})
+export class RafPafComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
